188cbe832a4846820203f1aac61ef617227bbed0
Avjit Mondal <avijitmondal38@gmail.com>
2021-05-06 13:45:25 +0530

    Added main method for java11

--- src/main/java/com/avijitmondal/practice/java/Java11.java
+++ src/main/java/com/avijitmondal/practice/java/Java11.java
@@ -2,6 +2,10 @@ package com.avijitmondal.practice.java;
 
 public class Java11 {
 
+    public static void main(String[] args) {
+        Java11 java11 = new Java11();
+        java11.test();
+    }
     public void test() {
         final var abc = new String("hello");
         System.out.println(abc);
